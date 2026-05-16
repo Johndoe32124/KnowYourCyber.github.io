bda77eea975432addf4f634787f80adf7737b327
Johndoe32124 <qpk924501@gmail.com>
2026-05-16 21:18:01 +0400

    Update apiConfig.js

--- scripts/api/apiConfig.js
+++ scripts/api/apiConfig.js
@@ -3,4 +3,4 @@
 // Stores the path to the JSON file for easy reuse
 // ============================================================
 
-export const API_URL = "../../database/breaches.json";
+export const API_URL = "https://raw.githubusercontent.com/Johndoe32124/KnowYourCyber.github.io/refs/heads/main/database/breaches.json";
